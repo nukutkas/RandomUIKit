d643d6cc775be741262c6f0b5dc91d4eb55f7d6a
Sosin Vitalii <375693@mail.ru>
2022-05-24 02:06:42 +0300

    Fix CustomPaddingCell

--- RandomUIKitExample/RandomUIKitExample/Modules/CellsScreenModule/Factory/CustomCells/CustomPadding/CustomPaddingCell.swift
+++ RandomUIKitExample/RandomUIKitExample/Modules/CellsScreenModule/Factory/CustomCells/CustomPadding/CustomPaddingCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 // MARK: - CustomPaddingCell
 
-public final class CustomPaddingCell: UITableViewCell {
+final class CustomPaddingCell: UITableViewCell {
     
     // MARK: - Public property
     
